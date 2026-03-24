8cb7d9a0285fd3ce0f2bef437c6afa14c746c319
tabletka97 <khatkevichmatsvei@gmail.com>
2026-03-24 23:05:55 +0300

    init commit

---
+++ src/main/java/com/innowise/orderservice/InnowiseJavaAdvancedCourseOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.innowise.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnowiseJavaAdvancedCourseOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnowiseJavaAdvancedCourseOrderServiceApplication.class, args);
+    }
+
+}
